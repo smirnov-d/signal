3c6a04423453b803f16ced3444ab17f5b0f890ff
Dmitry <dim-on-94@mail.ru>
2020-07-08 17:15:58 +0300

    base server config

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const PORT = process.env.PORT || 3000
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('system server');
+  socket.on('system', (data) => {
+    console.log('offer', data);
+    io.emit('offer', data);
+  });
+  socket.on('systemanswer', (data) => {
+    console.log('system-answer', data);
+    io.emit('answer', data);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
