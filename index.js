e0e61f4b418e56b01a1e77e26eeb0138bbac2cf4
smirnov-d <dim-on-94@mail.ru>
2021-07-08 17:46:06 +0300

    cors

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 const PORT = process.env.PORT || 3000
 
 const app = require('express')();
+const cors = require('cors')
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+app.use(cors({
+  origin: process.env.WEB_URL || 'localhost',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}))
 
 io.on('connection', (socket) => {
   console.log('system server');
